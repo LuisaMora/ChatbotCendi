2705ce4892634e33b6a371f469ebe982689343ff
Gomer Claure <gomerclaureb@gmail.com>
2022-06-24 20:30:43 -0400

    Agregando el modelo a la API

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from train_chatbot import train
+from chatgui import chatbot_response
 
 app = Flask(__name__)
 
@@ -8,8 +10,22 @@ def get_respuesta():
 
 @app.route('/chatbot', methods=['POST'])
 def post_metodo():
-    return jsonify({"mensaje":"Recibi el mensaje"})
+    try:
+        mensaje = request.json
+        print(mensaje)
+        respuesta = chatbot_response(mensaje["mensaje"])
+        return jsonify({"mensaje":respuesta})
+    except Exception as e:
+        print(Exception)
+        return jsonify({"mensaje":"Error al obtener la respuesta"}),500
 
+@app.route('/chatbot/entrenar', methods=['GET'])
+def post_metodo_entrenar():
+    try:
+        train()
+        return jsonify({"mensaje":"Modelo entrenado"})
+    except:
+        return jsonify({"mensaje":"Error al entrenar el modelo"}),500
 
 if __name__ =="__main__":
     app.run(debug=True, port=5050)
