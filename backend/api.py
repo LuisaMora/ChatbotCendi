644761f2d67e7d333690ea11f988bcf73703064c
Gomer Claure <gomerclaureb@gmail.com>
2022-06-28 10:17:13 -0400

    Cambiando nombres de archivos

--- backend/api.py
+++ backend/api.py
@@ -4,9 +4,12 @@ from chatgui import ChatBot
 import json
 
 app = Flask(__name__)
+
+
 @app.route('/chatbot', methods=['GET'])
 def get_respuesta():
-    return jsonify({"mensaje":"Hola a todos"})
+    return jsonify({"mensaje": "Hola a todos"})
+
 
 @app.route('/chatbot/<index_no>', methods=['POST'])
 def post_metodo(index_no):
@@ -19,21 +22,23 @@ def post_metodo(index_no):
         print(f"-----*************{respuesta}")
 
         data_json = json.dumps(respuesta)
-        archivo_json = open("../frontend/data.json","w")
+        archivo_json = open("../frontend/data.json", "w")
         archivo_json.write(data_json)
         archivo_json.close()
-        return jsonify({"body":respuesta}),200
+        return jsonify({"body": respuesta}), 200
     except Exception as e:
         print(e)
-        return jsonify({"mensaje":"Error al obtener la respuesta"}),500
+        return jsonify({"mensaje": "Error al obtener la respuesta"}), 500
+
 
 @app.route('/chatbot/entrenar', methods=['GET'])
 def post_metodo_entrenar():
     try:
         entrenar_modelo()
-        return jsonify({"mensaje":"Modelo entrenado"})
+        return jsonify({"mensaje": "Modelo entrenado"})
     except:
-        return jsonify({"mensaje":"Error al entrenar el modelo"}),500
+        return jsonify({"mensaje": "Error al entrenar el modelo"}), 500
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True, port=5050)
--- backend/entrenador.py
+++ backend/entrenador.py
@@ -1,44 +1,28 @@
-from tensorflow.keras.optimizers import SGD
-from tensorflow.keras.layers import Dense,Dropout
-from tensorflow.keras.models import Sequential
-import random
-import numpy as np
-from nlp_procesamiento import lemmatizar
+import pickle
+import json
+from nlp_procesamiento import tokenizar, lemmatizar
+from entrenador import procesado_datos, entrenar
 
-def procesado_datos(palabras_lematizadas,etiquetas_relacionadas, etiquetas):
-    salidas_vacias = [0] * len(etiquetas)
-    datos_de_entrenamiento = []
-    for palabras_etiqueta in etiquetas_relacionadas:
-        patron_palabras = palabras_etiqueta[0]
-        patron_palabras = lemmatizar(patron_palabras)
-        bag_pal = [ 1 if palabra in patron_palabras else 0 for palabra in palabras_lematizadas]
+def entrenar_modelo():
+    palabras_tokenizadas = []
+    palabras_lematizadas = []
+    etiquetas = []
+    etiquetas_relacionadas = []
+    archivo_patrones = open('./preguntas.json').read()
 
-        bag_etiq = list(salidas_vacias)
-        bag_etiq[etiquetas.index(palabras_etiqueta[1])] = 1
+    dict_patrones = json.loads(archivo_patrones)
+    for textos in dict_patrones['preguntas']:
+        p, etiqueta, etiq_rel, = tokenizar(textos["patterns"], textos["tag"])
+        palabras_tokenizadas += p
+        etiquetas += etiqueta
+        etiquetas_relacionadas += etiq_rel
+    palabras_lematizadas = sorted(list(set(lemmatizar(palabras_tokenizadas))))
+    etiquetas = sorted(list(set(etiquetas)))
+    pickle.dump(palabras_lematizadas, open('palabras_lematizadas.pkl', 'wb'))
+    pickle.dump(etiquetas, open('etiquetas.pkl', 'wb'))
+    train_x,train_y = procesado_datos(palabras_lematizadas,etiquetas_relacionadas,etiquetas)
+    entrenar(train_x,train_y)
 
-        datos_de_entrenamiento.append([bag_pal, bag_etiq])
 
-    random.shuffle(datos_de_entrenamiento)
-    datos_de_entrenamiento = np.array(datos_de_entrenamiento)
-   
-    train_x = list(datos_de_entrenamiento[:, 0])
-    train_y = list(datos_de_entrenamiento[:, 1])
-    return train_x,train_y
-
-def entrenar(entrenamiento_palabras,entrenamiento_etiquetas):
-  
-    modelo_s = Sequential()
-    modelo_s.add(Dense(128, input_shape=(len(entrenamiento_palabras[0]),), activation='relu'))
-    modelo_s.add(Dropout(0.5))
-    modelo_s.add(Dense(64, activation='relu'))
-    modelo_s.add(Dropout(0.5))
-    modelo_s.add(Dense(len(entrenamiento_etiquetas[0]), activation='softmax'))
-
-    
-    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    modelo_s.compile(loss='categorical_crossentropy',
-                  optimizer=sgd, metrics=['accuracy'])
-
-    hist = modelo_s.fit(np.array(entrenamiento_palabras), np.array(entrenamiento_etiquetas),
-                     epochs=200, batch_size=5, verbose=1)
-    modelo_s.save('modelo_generado.h5', hist)
+if __name__ == "__main__":
+    entrenar_modelo()
